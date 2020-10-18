a111186e2ee7ab8c17f5c5d3a9d39ca1fca13e61
LIN LUO <linluo@student.unimelb.edu.au>
2020-10-18 22:33:19 +0800

    iid

--- api/src/main/java/com/medsec/util/Database.java
+++ api/src/main/java/com/medsec/util/Database.java
@@ -479,7 +479,7 @@ public class Database {
             System.out.println(id);
             System.out.println("nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
             ResourceMapper mapper=session.getMapper(ResourceMapper.class);
-            mapper.deleteResource(id);
+            mapper.deleteAPPResource(id);
             System.out.println("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
             session.commit();
         } finally {
